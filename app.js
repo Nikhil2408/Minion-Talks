bfa3402789cb92c703d4a3598c60e1c8da62ccd4
Nikhil Bansal <nikbansal1998@gmail.com>
2021-10-16 22:16:10 -0900

    feat: Added translation

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const btn = document.querySelector(".btn-primary");
 const inputText = document.querySelector("textarea");
 const outputText = document.querySelector("input[type='text']");
 
+// const url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+const url = "https://api.funtranslations.com/translate/minion.json";
+
+function doTranslation(text)
+{
+    let finalURL = url + "?text=" + text;
+    fetch(finalURL)
+    .then(response => response.json())
+    .then(json => outputText.value = json.contents.translated);
+}
+
 btn.addEventListener("click", function clickEventListener()
 {
-    console.log("Primary Button Clicked!!!");
-    console.log(`Value of input field is ${inputText.value}`);
-    outputText.value = inputText.value;
+    doTranslation(inputText.value);
 });
 
 
